8b1035ba262819f837ee0440a3c6a899747d659d
augustomorales92 <augustomorales92@gmail.com>
2025-03-02 12:04:21 +0100

    refactor: enhance ThemeProvider to set default theme and enable system theme switching

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,10 @@
-"use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+'use client'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </NextThemesProvider>
+  )
 }
-
